4ef47fcc670cbac9da97575bbd5b3131ecc2a6da
cobb380 <cobbsalad380@gmail.com>
2024-07-02 19:21:12 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:show]
+  root to: "prototypes#index"
+  resources :prototypes do
+    resources :comments, only: [:create, :show]
+  end
+end
